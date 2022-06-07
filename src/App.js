e98f320530f4f38795c2e3b53ad4600a01a01f72
Illia Heria <i.heria@andersenlab.com>
2022-06-07 21:37:17 +0300

    first commit of intense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Ticker from "./components/Ticker/Ticker";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+  hide = () => {
+    this.setState({ show: false });
+  };
+  render() {
+    return (
+      <>
+        <Header title="From second one" />
+        <Ticker hide={this.hide} />
+        {/* {/* <Header title="Hello team" /> */}
+      </>
+    );
+  }
 }
 
 export default App;
