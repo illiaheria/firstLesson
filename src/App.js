bf2ef86ba77b408ddf11b4a3c34c58ec252911b5
Illia Heria <i.heria@andersenlab.com>
2022-06-09 20:34:30 +0300

    second lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Ticker from "./components/Ticker/Ticker";
+import { usersList } from "./mocks/itemsList";
+import List from "./components/List/List";
+import { ThemeContext, themes } from "./utils/ThemeContex";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true,
+      items: usersList,
+      theme: themes.dark,
     };
   }
-  hide = () => {
-    this.setState({ show: false });
+  handleDelete = (itemId) => {
+    this.setState((state) => {
+      return { items: state.items.filter((item) => item.id !== itemId) };
+    });
   };
+
+  toggleTheme = () => {};
+
   render() {
     return (
-      <>
-        <Header title="From second one" />
-        <Ticker hide={this.hide} />
-        {/* {/* <Header title="Hello team" /> */}
-      </>
+      <ThemeContext.Provider value={this.state.theme}>
+        <div className="mainContainer">
+          <List items={this.state.items} deleteHandler={this.handleDelete} />
+        </div>
+      </ThemeContext.Provider>
     );
   }
 }
